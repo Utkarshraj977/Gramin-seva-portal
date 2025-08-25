909c89a73a8ba23ae00ecdd6988040bd6979e81b
utkarsh raj <utkarshbrb6@gmail.com>
2025-08-26 00:18:42 +0530

    frontend and backend is merged succesfully and ready for frontend code

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    return <h1>Gramin Vikas Portal</h1>;
+}
+
+export default App
--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,6 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-
 const app = express()
 
 app.use(cors({
@@ -21,11 +20,20 @@ import doctorRouter from './routes/doctor.routes.js'
 import patientRouter from './routes/patient.routes.js'
 import educationRouter from './routes/education.routes.js'
 import studentRouter from './routes/student.routes.js'
+import  ComplaintAdmin  from "./routes/complaintAdmin.routes.js"
+import  ComplaintUser  from './routes/complaintuser.routes.js'
+import  travelleradmin  from './routes/travelleradmin.routes.js'
+import  travelleruser  from './routes/travelleruser.routes.js'
 
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/doctor",doctorRouter)
 app.use("/api/v1/patient",patientRouter)
 app.use("/api/v1/education",educationRouter)
 app.use("/api/v1/student",studentRouter)
+app.use("/api/v1/ComplaintAdmin", ComplaintAdmin)
+app.use("/api/v1/complaintuser",ComplaintUser)
+app.use("/api/v1/traveller",travelleradmin)
+app.use("/api/v1/traveller",travelleruser)
 
 export {app} ;
+
