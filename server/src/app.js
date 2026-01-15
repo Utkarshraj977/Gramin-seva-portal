83c3b9a89693fbbc44db2a12c38279ea6ac0b84b
SAMEER KUSHWAHA <sameerkushwaha563@gmail.com>
2026-01-15 18:33:13 +0530

    Fix syntax error in /test route handler

--- server/src/app.js
+++ server/src/app.js
@@ -28,9 +28,10 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.get(/test,(req,res)=>{
-       res.send('hieyujdydf') 
-)
+app.get("/test", (req, res) => {
+    res.send("hi yujdydf");
+});
+
 
 // --- Routes Imports ---
 import userRouter from './routes/user.routes.js'
