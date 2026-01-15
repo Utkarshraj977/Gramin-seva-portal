110108206411d7a1431f1a71d384b0e505209db3
SAMEER KUSHWAHA <sameerkushwaha563@gmail.com>
2026-01-15 18:26:59 +0530

    Add GET route to app.js
    Add a new GET route that responds with a string.

--- server/src/app.js
+++ server/src/app.js
@@ -28,7 +28,9 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
-
+app.get(/,(req,res)=>{
+       res.send('hieyujdydf') 
+)
 
 // --- Routes Imports ---
 import userRouter from './routes/user.routes.js'
