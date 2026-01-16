686c7b2e69a1b797bd6e6476f2d685595a0cd947
SAMEER KUSHWAHA <sameerkushwaha563@gmail.com>
2026-01-16 16:43:34 +0530

    Enhance JWT verification with logging
    Added logging for received cookies and authorization header.

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -6,12 +6,13 @@ import { User } from "../models/user.model.js";
 export const verifyJWT = asyncHandler(async(req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        
+        console.log("Cookies Received:", req.cookies); 
+    console.log("Auth Header:", req.header("Authorization"));
         // console.log(token);
         if (!token) {
             throw new ApiError(401, "Unauthorized request")
         }
-    
+        
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
     
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
