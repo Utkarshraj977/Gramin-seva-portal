587baee69e17e70170144662383eeef7904603ba
Utkarsh Raj <utkarshbrb6@gmail.com>
2026-08-30 15:35:07 +0530

    chore: clean repository and improve documentation

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
-        console.log("MONGODB connection FAILED ", error);
-        process.exit(1)
-    }
-}
-export default connectDB
+  try {
+    const connection = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
+    );
+
+    console.log(`MongoDB connected: ${connection.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
